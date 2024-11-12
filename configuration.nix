e3434e7d1a1d5585701944144a8244cb11c3d9ae
tabung <yurandarezky@atmaluhur.ac.id>
2024-11-12 10:39:06 +0700

    apalah

--- configuration.nix
+++ configuration.nix
@@ -60,10 +60,12 @@
   services.printing.enable = true;
 
   # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
 
