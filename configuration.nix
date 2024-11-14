a375d799229789ff85a37352878a7bd0f221dde1
tabung <yurandarezky@atmaluhur.ac.id>
2024-11-14 17:53:00 +0700

    *

--- configuration.nix
+++ configuration.nix
@@ -18,24 +18,10 @@
   boot.supportedFilesystems = [ "ntfs" ];
 
   networking.hostName = "devzc0de"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Asia/Jakarta";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
--- users/apps/code/vscode.nix
+++ users/apps/code/vscode.nix
@@ -4,11 +4,6 @@
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-      zhuangtongfa.Material-theme
-      ms-vscode.sublime-keybindings
-      oderwat.indent-rainbow
     ];
   };
 }
