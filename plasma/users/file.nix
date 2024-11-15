4efb3687032ea3cecf8efcd6cc853ccae5de9f8e
Rezky Yuranda <yurandarezky@atmaluhur.ac.id>
2024-11-15 14:11:23 +0700

    move

---
+++ plasma/users/file.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+let
+  plasmaConfigDir = "../systems/ui-environment/plasma/config";
+  swayConfigDir = "../systems/ui-environment/sway/config";
+in
+{
+  # Menggunakan kondisi untuk menentukan DE mana yang aktif
+}
